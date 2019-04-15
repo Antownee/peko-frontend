87cad4750e63587ebdc4c8979f3a548701ca5cca
Anthony Muisyo <muisyoanthony@gmail.com>
2019-04-15 10:56:38 +0300

    Added tea card selection handler

--- src/views/PlaceOrder.js
+++ src/views/PlaceOrder.js
@@ -1,12 +1,13 @@
 import React from "react";
-import {Link} from "react-router-dom"
-import { 
-  Container, 
-  Row, 
-  Col, 
+import { Link } from "react-router-dom"
+import {
+  Container,
+  Row,
+  Col,
   Card,
   CardBody,
-  Badge } from "shards-react";
+  Badge
+} from "shards-react";
 
 import PageTitle from "../components/common/PageTitle";
 import UserAccountDetails from "../components/user-profile-lite/UserAccountDetails";
@@ -15,63 +16,70 @@ class PlaceOrder extends React.Component {
   constructor(props) {
     super(props);
 
-    this.teaListItemClickable = this.teaListItemClickable.bind(this);
+    //this.teaListItemClickable = this.teaListItemClickable.bind(this);
 
     this.state = {
-      selectedTeaItem: 1,
+      selectedTeaItem: null,
       teaList: [
         {
+          index: 0,
           backgroundImage: require("../images/content-management/17.jpg"),
-          category: "Business",
-          categoryTheme: "dark",
+          category: "Selected",
+          categoryTheme: "warning",
           title: "Black Tea",
           body:
             "To be brewed in 100 degrees boiling water",
-          date: "28 February 2019"
+          date: "28 February 2019",
+          selected: false
         },
         {
+          index: 1,
           backgroundImage: require("../images/content-management/17.jpg"),
-          category: "Travel",
-          categoryTheme: "info",
+          category: "Selected",
+          categoryTheme: "warning",
           title: "Green Tea",
           body:
             "To be brewed in 100 degrees boiling water",
-            date: "29 February 2019"
+          date: "29 February 2019",
+          selected: false
         },
         {
+          index: 2,
           backgroundImage: require("../images/content-management/17.jpg"),
-          category: "Technology",
-          categoryTheme: "royal-blue",
+          category: "Selected",
+          categoryTheme: "warning",
           title: "Yellow Tea",
           body:
             "To be brewed in 100 degrees boiling water",
-            date: "29 February 2019"
+          date: "29 February 2019",
+          selected: false
         },
         {
+          index: 3,
           backgroundImage: require("../images/content-management/17.jpg"),
-          category: "Business",
+          category: "Selected",
           categoryTheme: "warning",
           author: "John James",
           title: "White Tea",
           body:
             "To be brewed in 100 degrees boiling water",
-            date: "29 February 2019"
+          date: "29 February 2019",
+          selected: true
         }
       ],
     }
-    
+
 
   }
 
-  teaListItemClickable(e){
-    console.log("Clicked");
-    console.log(e)
-    //change badge colour
-    //this.state.selectedTeaItem
+  teaListItemClickable(index) {
+    this.setState({
+      selectedTeaItem: index
+    })
   }
 
-//Card clickable callback onClick={siteSelectedCallback}    
-         
+  //Card clickable callback onClick={siteSelectedCallback}    
+
 
   render() {
     return (
@@ -82,31 +90,38 @@ class PlaceOrder extends React.Component {
         <Row>
           {this.state.teaList.map((post, idx) => (
             <Col lg="3" md="6" sm="12" className="mb-4" key={idx}>
-           
-            <a style={{ cursor: 'pointer' }} onClick={this.teaListItemClickable}>
-              <Card small className="card-post card-post--1">
-                <div
-                  className="card-post__image"
-                  style={{ backgroundImage: `url(${post.backgroundImage})` }}
-                >
-                  <Badge
-                    pill
-                    className={`card-post__category bg-${post.categoryTheme}`}
+
+              <a style={{ cursor: 'pointer' }} onClick={this.teaListItemClickable.bind(this, idx)}>
+                <Card small className="card-post card-post--1">
+                  <div
+                    className="card-post__image"
+                    style={{ backgroundImage: `url(${post.backgroundImage})` }}
                   >
-                    {post.category}
-                  </Badge>
-                  
-                </div>
-                <CardBody>
-                  <h5 className="card-title">
-                    <a href="#" className="text-fiord-blue">
-                      {post.title}
-                    </a>
-                  </h5>
-                  <p className="card-text d-inline-block mb-3">{post.body}</p>
-                  <span className="text-muted">{post.date}</span>
-                </CardBody>
-              </Card>
+
+                    {
+                      idx === this.state.selectedTeaItem ?
+                      <Badge
+                        pill
+                        className={`card-post__category bg-${post.categoryTheme}`}
+                      >
+                        {post.category}
+                      </Badge>
+                      : ""
+                    }
+
+
+
+                  </div>
+                  <CardBody>
+                    <h5 className="card-title">
+                      <a href="#" className="text-fiord-blue">
+                        {post.title}
+                      </a>
+                    </h5>
+                    <p className="card-text d-inline-block mb-3">{post.body}</p>
+                    <span className="text-muted">{post.date}</span>
+                  </CardBody>
+                </Card>
               </a>
             </Col>
           ))}
